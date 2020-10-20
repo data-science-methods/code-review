ed3292251ff2bfe00dd2cdbf67194e82b8173ae9
Jingo <jingo.wins@gmail.com>
2020-10-20 14:51:39 -0700

    Cut some redundancy and fixed a bug
    Bug - dataf manipulation order now doesn't break out of the box

--- SportsStats_ImplicitBias/eda.R
+++ SportsStats_ImplicitBias/eda.R
@@ -3,36 +3,45 @@ library(ggplot2)
 library(vroom) 
 library(skimr) 
 library(visdat)
-library(tictoc) 
 
 #load data
 data_dir = 'data'
 target_file = file.path(data_dir, 'CrowdstormingDataJuly1st.csv')
-tic()
-read.csv(target_file)
-toc()
+
 dataf_raw = vroom(target_file)
 skim(dataf_raw)
 
-#checking for missing values
+#check for missing values
 set.seed(2020-09-10)
 dataf_smol = sample_n(dataf_raw, 1000)
 vis_miss(dataf_smol, cluster = TRUE)
 
+#set cleaned dataframe value
 #remove missing values
+#add rater_mean column
 dataf = dataf_raw %>% 
   filter(!is.na(photoID),!is.na(rater1),!is.na(rater2)) %>% 
-  mutate(rater_mean = rowMeans(dataf[,c('rater1', 'rater2')]))
+  mutate(rater_mean = rowMeans(select(.,rater1, rater2)))
 
 #check for clean data
-  set.seed(2020-09-10)
-  dataf_smol = sample_n(dataf, 1000)
-  vis_miss(dataf_smol, cluster = TRUE)
+set.seed(2020-09-10)
+dataf_smol = sample_n(dataf, 1000)
+vis_miss(dataf_smol, cluster = TRUE)
   
 #rater correlation
-  with(dataf, cor(rater1, rater2))
+with(dataf, cor(rater1, rater2))
   
 #red cards and race
-  with(dataf, cor(rater_mean, redCards))
-  ggplot(dataf, aes(rater_mean, redCards)) +
-    geom_point() 
+with(dataf, cor(rater_mean, redCards))
+
+#visualizing
+
+#skin colors
+ggplot(dataf) + 
+  geom_bar(aes(x = rater_mean))
+
+#
+ggplot(dataf) + 
+  geom_bar(aes(x = redCards))
+ggplot(dataf) + 
+  geom_bar(aes(x = yellowCards))
