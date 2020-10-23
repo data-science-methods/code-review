fa6e30add02103456806ae0a67e1d5d4c92ab30d
Butovens Médé <bmede@ucmerced.edu>
2020-10-22 19:51:50 -0700

    added DESCRIPTION file and first part of script

--- OurScript.R
+++ OurScript.R
@@ -1,15 +1,39 @@
+## Loading Necessary Libraries
 library(tidyverse)
 library(skimr)
 library(visdat)
+library(ggplot2)
 
-data = read_csv("1. Crowdsourcing Dataset July 01, 2014 Incl.Ref Country/CrowdstormingDataJuly1st.csv")
 
+## Load data
+data = read_csv("CrowdstormingDataJuly1st.csv")
 
+## Skim the data
 skim(data)
+
+## Visualization of the data
+vis_miss(data, warn_large_data = FALSE)
+
+
+smallData <- data %>%  
+  select(club, leagueCountry, goals, victories, defeats, redCards, yellowCards, ties) 
+
+smallData %>% 
+  select(goals, victories, defeats, redCards, yellowCards, ties) %>% 
+  vis_cor()
+
+smallData %>% 
+  group_by(leagueCountry) %>% 
+  summarize(n = n(),
+            across(.cols = redCards,
+                   .fns = lst(mean, sd),
+                   aov() ))
+
+
 
 hist(data$rater1)
 hist(data$rater2)
 
-vis_miss(data, warn_large_data = FALSE)
+
 
 
